bad0d754761227b6ef790de9bfcb0fd481458eb7
smishra1000 <smishra1000@rediffmail.com>
2024-06-28 20:56:26 +0530

    objcet sand function

---
+++ javascript-session/function-object.js
@@ -0,0 +1,168 @@
+// let a = 10;
+// let b = 20;
+
+// let c = a+b;
+
+// console.log(c)
+
+// function is block  of  code use to perform specific task 
+
+// function sum() {
+//     let a = 10;
+//     let b = 20;
+//     let c = a + b;
+//     console.log(c)
+// }
+
+// sum();
+// sum();
+// sum();
+// arguments,  parameter
+
+// function greet(msg,name){
+//     console.log(msg,name)
+// }
+
+// greet("good morning","test")
+
+// greet("good evening","sameer")
+
+// function chekEvenOrOdd(n) {
+//     if (n % 2 === 0) {
+//         console.log("number is even")
+//     } else {
+//         console.log("number is odd")
+//     }
+// }
+
+// chekEvenOrOdd(10)
+// chekEvenOrOdd(21)
+
+
+
+// function checkEven(n) {
+//     // let n = 10;
+//     if (n % 2 === 0) {
+//         console.log("number is even")
+//     } else {
+//         console.log("number is odd")
+//     }
+// }
+
+// checkEven(10);
+
+// checkEven(100);
+
+// checkEven(21)
+
+
+// function can return the value
+
+// function calculate(a, b) {
+//     return a + b
+// }
+
+
+// let c = calculate(10,20)
+// console.log(c)
+
+// let userPaid = false
+// function checkSubscription(){
+
+//     // return "done"
+//     console.log("printing hiiiii")
+//     // if(userPaid===true){
+//     //     return "user already paid"
+//     // }else{
+//     //     return "user not paied"
+//     // }
+// }
+
+// let result = checkSubscription()
+
+// console.log(result)
+
+
+// function greet(){
+//     console.log("hiii");
+//     console.log("hello")
+//     return {id:2}
+//     console.log("good morning")
+//     console.log("good evening")
+// }
+
+// let r = greet();
+// console.log("returend successfullly",r)
+
+// function isVaildVoter(age){
+//     if(age>18){
+//        return "valid voter"
+//     }else{
+//        return "not a valid voter"
+//     }
+// }
+
+// console.log(isVaildVoter(10))
+
+// wap to cehck number is positive negative or zero
+
+
+// function checkNumber() {
+//     let n = Number(prompt("enter any number"))
+//     if (n > 0) {
+//         console.log("number is positive")
+//     } else if (n < 0) {
+//         console.log("number is negative")
+//     } else {
+//         console.log("number is zero")
+//     }
+// }
+
+// checkNumber()
+// checkNumber()
+
+
+// function checkNumber2() {
+//     let n = Number(prompt("enter any number"))
+//     switch (Math.sign(n)) {
+//         case 1:
+//             console.log("number is positive")
+//             break;
+//         case -1:
+//             console.log("number is negative")
+//             break;
+//         default:
+//             console.log("zero")    
+//     }
+// }
+
+// checkNumber2()
+
+
+/// retrun function from function
+
+
+// function greet() {
+//     return function sayHi(){
+//         console.log("hhiii")
+//     }
+// }
+
+// let result = greet();
+
+// console.log(result())
+
+
+// function sayHiii(){
+//     console.log("hiii")
+// }
+
+// let x = sayHiii;
+
+// console.log(x())
+
+
+
+
+
+
---
+++ javascript-session/object.js
@@ -0,0 +1,47 @@
+// object is simply variable wich containe key value pair; 
+
+
+
+// object literal
+// let obj = {
+//     name:"test1",
+//     age:20,
+//     address:"hyd"
+// }
+
+// obj.email="test1@gmail.com"
+
+//  console.log(obj)
+// console.log(obj.name)
+// console.log(obj.age)
+
+// console.log(obj.address)
+
+// console.log(obj["age"])
+
+// for(let key in obj){
+//     console.log(key,obj[key])
+// }
+
+// let age = 20
+// let name="sameer"
+let email = "test@gmail.com"
+let user = {
+    age:20,
+    name:"sameer",
+    email:email
+}
+
+// console.log(user.age)
+// console.log(user.name)
+
+for(let x in user){
+    console.log(x,user[x])
+}
+
+function,object--
+
+
+// wap to find reverse of number-- 123   321
+// wap to check number is palidrome or not---  123  321  121---121
+// wap to find digit of number--- 1234--- 1 2 3 4,  578   8 7 5
