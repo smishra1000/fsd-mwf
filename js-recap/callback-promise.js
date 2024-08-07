e4b7508f2fe7bd2774c3fa329c594a529818306c
smishra1000 <smishra1000@rediffmail.com>
2024-08-07 20:41:09 +0530

    js recap

--- js-recap/callback-promise.js
+++ js-recap/callback-promise.js
@@ -29,14 +29,116 @@
 // let users = getUsers(displayUsers);
 
 
-function getName(){
-    return "sameer"
-}
+// function getName(){
+//     return "sameer"
+// }
 
-function greetMe(name){
-    console.log("hi good mroning",name)
-}
-let name = getName();
-greetMe(name)
+// function greetMe(name){
+//     console.log("hi good mroning",name)
+// }
+// let name = getName();
+// greetMe(name)
 
+
+
+//callback--
+// let student = {
+//     show:function(){
+//         console.log("hii")
+//     }
+// }
+
+
+// function show(){
+//     console.log("hii")
+// }
+
+// console.log(student.show())
+
+
+
+
+
+// function fetchData(callback){
+//     setTimeout(()=>{
+       
+//         callback("hi data")
+//     },2000)
+// }
+
+// function printData(data){
+//     console.log(data)
+// }
+
+// // let result = fetchData();// so while executing this js engine will pick printData
+// // printData(result);
+
+// fetchData(printData)
+
+
+
+// // task 1 task 2
+
+// uisng promise---
+
+
+
+// function fetchData(){
+//     let isNetwork = false
+//     return new Promise(((resolve,reject)=>{
+//         setTimeout(()=>{
+//             if(isNetwork){
+//                 resolve("hi data")
+//             }else{
+//                 reject("no netwrok available")
+//             }
+            
+//         },2000)
+//     }))
+// }
+
+// function printData(data){
+//     console.log(data)
+// }
+
+// fetchData().then((data)=>{
+//     printData(data)
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+// function getUsers(){
+//     return new Promise((resolve,reject)=>{
+//         fetch("https://jsonplaceholder.typicode.com/users").then((res)=>{
+//             return res.json()
+//         }).then((data)=>{
+//             resolve(data)
+//         })
+//     })
+   
+// }
+
+// function printUsers(users){
+//     console.log("users recieved=",users)
+// }
+
+// getUsers().then((res)=>{
+//     printUsers(res)
+// })
+
+
+
+// using async await
+
+// async function getUsers(){
+//     let data = await fetch("https://jsonplaceholder.typicode.com/users");
+//     let users = await data.json();
+//     printUsers(users)
+// }
+
+// getUsers();
+
+// function printUsers(users){
+//     console.log("users recieved=",users)
+// }
 
---
+++ js-recap/es6.js
@@ -0,0 +1,50 @@
+
+// 
+
+// 1 . let, const
+
+// {
+//     let x  =1000;
+//     console.log(x)
+// }
+
+// console.log(x)
+
+
+//2. ``
+// let x =100
+// let y =2000;
+
+ //let val = "value is " + x + "next y value is " + y;
+// let val = `value is ${x} next y value is ${y}`
+// console.log(val)
+
+// // rest and spread operator ... 
+
+// // let obj1 = {
+// //     id:101,
+// //     age:20
+// // }
+
+// // let obj2 = {...obj1,email:"test1@gmail.com"}
+
+// // console.log(obj2)
+
+// function add(a,b,...c){
+//     console.log(a,b,c)
+// }
+
+// add(2,3,4,5,6,7)
+
+
+//  let arr1 = [1,2,3,4,5]
+
+// // let arr2 = [6,7,8,9]
+
+// // let result = [...arr1,...arr2]
+
+// // console.log(result)
+
+// let [a,...b] = arr1;
+
+// console.log(a,b)
